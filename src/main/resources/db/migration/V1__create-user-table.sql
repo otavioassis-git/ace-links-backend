8104345fb8467150b449895adcb9e311c285975b
otavioassis-git <otavioassis2013@gmail.com>
2025-07-02 15:16:29 -0300

    feat: creating initial user table migration

---
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    avatar VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    role VARCHAR(255) NOT NULL
+);
