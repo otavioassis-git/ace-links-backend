4226f923cf8282da3151efd769c1c80ecffbed00
otavioassis-git <otavioassis2013@gmail.com>
2025-07-02 15:20:46 -0300

    refact: adding description column to user table

--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -7,6 +7,7 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     avatar VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     role VARCHAR(255) NOT NULL
