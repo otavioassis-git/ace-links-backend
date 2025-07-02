8d40ad39cfe8f0489cde97981c1ccc727738623f
otavioassis-git <otavioassis2013@gmail.com>
2025-07-02 14:45:46 -0300

    initial commit

---
+++ src/main/java/api/ace_links/AceLinksApplication.java
@@ -0,0 +1,13 @@
+package api.ace_links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/ace_links/AceLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ace_links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
