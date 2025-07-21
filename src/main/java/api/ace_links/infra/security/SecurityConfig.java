47f972744093c77ddaea6c1d1ff31cca621bc855
otavioassis-git <otavioassis2013@gmail.com>
2025-07-21 16:54:06 -0300

    refact: security mapping for OPTION requests

--- src/main/java/api/ace_links/infra/security/SecurityConfig.java
+++ src/main/java/api/ace_links/infra/security/SecurityConfig.java
@@ -26,10 +26,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/{username}/links").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
